ffe8adf1e3a6630f5e8f2d128f7ac2c536b8f8e5
Shariar Islam <shariarn85@gmail.com>
2024-02-19 12:59:33 +0600

    simple redux counter app created

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,27 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "../redux/features/counter/counterSlice";
 
 const Counter = () => {
-    return (
-        <div>
-            <h2>Counter</h2>
-        </div>
-    );
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Counter</h2>
+
+      <div>
+              <button onClick={() => dispatch(increment())}>Increment</button>
+              <button onClick={() => dispatch(incrementByValue(5))}>Increment By 5</button>
+              
+        <h2>{count}</h2>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Counter from "../components/Counter";
 
 const Home = () => {
     return (
         <div>
             <h2>home</h2>
+            <Counter></Counter>
         </div>
     );
 };
